a48129eb9f6e3859f1d303c70c59e908c8353419
Qetevani21 <qetevani.basharuli@gmail.com>
2024-03-22 23:41:56 +0400

    add slider

--- JAVASCRIPT/script.js
+++ JAVASCRIPT/script.js
@@ -18,92 +18,27 @@ document.querySelectorAll(".a-item").forEach((n) =>
 
 //slider
 
-
-// // registration
-// export function validationForm() {
-//   let form = document.getElementById("formelement");
-
-//   form.addEventListener("submit", function (event) {
-//     event.preventDefault();
-
-//     let error = {};
-
-//     //username
-
-//     let usernameValue = document.getElementById("name").value;
-
-//     if (usernameValue == "") {
-//       errors.name = "Name field cannot be empty";
-//     }
-//     console.log(error);
-
-//     this.querySelectorAll(".error-text").forEach((el) => {
-//       el.textContent = "";
-//     });
-
-//     for (let item in error) {
-//       console.log(item);
-
-//       let errorTextElement = document.getElementById("error_" + item);
-
-//       console.log(errorTextElement);
-
-//       if (errorTextElement) {
-//         errorTextElement.textContent = error[item];
-//       }
-//     }
-
-//     //message
-
-//     let messageValue = document.getElementById("message").value;
-
-//     if (messageValue == "") {
-//       error.message = "Feedback field cannot be empty";
-//     }
-//     console.log(error);
-
-//     this.querySelectorAll(".error-text").forEach((el) => {
-//       el.textContent = "";
-//     });
-
-//     for (let item in error) {
-//       console.log(item);
-
-//       let errorTextElement = document.getElementById("error_" + item);
-
-//       console.log(errorTextElement);
-
-//       if (errorTextElement) {
-//         errorTextElement.textContent = error[item];
-//       }
-//     }
-
-//     if (Object.keys(error).length === 0) {
-//       this.submit();
-//     }
-//   });
-
-//   //email regex
-
-//   let email = document.getElementById("emailfield");
-
-//   function validation() {
-//     let emailValue = document.getElementById("emailfield").value;
-//     let textError = document.getElementById("error-email");
-//     let emailPattern = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-
-//     if (emailValue.match(emailPattern)) {
-//       textError.textContent = "Your Email is Valid";
-//       textError.style.color = "green";
-//     } else {
-//       textError.textContent = "Your Email is Invalid";
-//       textError.style.color = "red";
-//     }
-
-//     if (emailValue == "") {
-//       textError.innerHTML = "";
-//     }
-//   }
-
-//   email.addEventListener("keyup", validation);
-// }
+let progressCircle = document.querySelector(".autoplay-progress svg");
+let progressContent = document.querySelector(".autoplay-progress span");
+let swiper = new Swiper(".mySwiper", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev"
+  },
+  on: {
+    autoplayTimeLeft(s, time, progress) {
+      progressCircle.style.setProperty("--progress", 1 - progress);
+      progressContent.textContent = `${Math.ceil(time / 1000)}s`;
+    }
+  }
+});
